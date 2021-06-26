473bcb15542534975640b31e43b76b9e882b611b
LeonardoConstantino <76814929+LeonardoConstantino@users.noreply.github.com>
2021-06-25 21:43:56 -0300

    organizando o código

--- script/app.js
+++ script/app.js
@@ -6,10 +6,55 @@ let listaSequenciaMaquina = []
 let listaSequenciaJogador = []
 let ini = 0
 
-AUDIOS[4].play()
-
 function add_random_numb_seq_maq() {
     listaSequenciaMaquina.push(Math.floor(Math.random() * 4))
+}
+
+function tocar_audio_correto(audio) {
+    AUDIOS[audio].currentTime = '-0.5'
+    AUDIOS[audio].play()
+}
+
+function liberar_p_jogador(poiter) {
+    for (let i = 0; i < BTN_CORES.length; i++) {
+        BTN_CORES[i].style.pointerEvents = poiter
+    }
+}
+
+function jogador_nao_completou_sequencia() {
+    if (listaSequenciaJogador.length < listaSequenciaMaquina.length) {
+        return true
+    }
+    return false
+}
+
+function zerar() {
+    listaSequenciaMaquina = []
+    listaSequenciaJogador = []
+    ini = 0
+    BTN_INICIAR.value = 'Iniciar'
+}
+
+function jogada_valida() {
+    for (let i = 0; i < listaSequenciaMaquina.length; i++) {
+        if (listaSequenciaJogador[i] !== listaSequenciaMaquina[i]) {
+            return false
+        }
+    }
+    listaSequenciaJogador = []
+    return true
+}
+
+function resutado_da_jogada(bntClicado) {
+    listaSequenciaJogador.push(bntClicado)
+    tocar_audio_correto(bntClicado)
+    if (jogada_valida()) {
+        add_random_numb_seq_maq()
+        mostrar_seq_nos_btn()
+        return
+    }
+    if (jogador_nao_completou_sequencia()){return}
+    fim_do_jogo()
 }
 
 function mostrar_seq_nos_btn() {
@@ -25,9 +70,18 @@ function mostrar_seq_nos_btn() {
     }, 1000)
 }
 
-function tocar_audio_correto(audio) {
-    AUDIOS[audio].currentTime = '-0.5'
-    AUDIOS[audio].play()
+function fim_do_jogo() {
+    AUDIOS[4].play()
+    liberar_p_jogador('none')
+    zerar()
+    let fim = 0
+    let intervaloFimDoJogo = setInterval(() => {
+        if (fim == 3) {clearInterval(intervaloFimDoJogo)}
+        for (let i = 0; i < BTN_CORES.length; i++) {
+            piscar_cor_botao(BTN_CORES[i])
+        }
+        ++fim
+    },200)
 }
 
 function piscar_cor_botao(botao) {
@@ -44,49 +98,6 @@ function piscar_cor_botao(botao) {
         botao.style.boxShadow = ''
         botao.style.filter = ''
     }, 300);
-}
-
-function liberar_p_jogador(poiter) {
-    for (let i = 0; i < BTN_CORES.length; i++) {
-        BTN_CORES[i].style.pointerEvents = poiter
-    }
-}
-
-function zerar() {
-    listaSequenciaMaquina = []
-    listaSequenciaJogador = []
-    ini = 0
-    BTN_INICIAR.value = 'Iniciar'
-}
-function jogador_nao_completou_sequencia() {
-    if (listaSequenciaJogador.length < listaSequenciaMaquina.length) {
-        return true
-    }
-    return false
-}
-
-function jogada_valida() {
-    for (let i = 0; i < listaSequenciaMaquina.length; i++) {
-        if (listaSequenciaJogador[i] !== listaSequenciaMaquina[i]) {
-            return false
-        }
-    }
-    listaSequenciaJogador = []
-    return true
-}
-
-function fim_do_jogo() {
-    AUDIOS[4].play()
-    liberar_p_jogador('none')
-    zerar()
-    let fim = 0
-    let intervaloFimDoJogo = setInterval(() => {
-        if (fim == 3) {clearInterval(intervaloFimDoJogo)}
-        for (let i = 0; i < BTN_CORES.length; i++) {
-            piscar_cor_botao(BTN_CORES[i])
-        }
-        ++fim
-    },200)
 }
 
 
@@ -108,48 +119,16 @@ for (let i = 0; i < BTN_CORES.length; i++) {
         const CLASSE_DO_BOTAO_CLICADO = e.target.classList[1]
         
         if (CLASSE_DO_BOTAO_CLICADO == 'verde') {
-            listaSequenciaJogador.push(0)
-            tocar_audio_correto(0)
-            if (jogada_valida()) {
-                add_random_numb_seq_maq()
-                mostrar_seq_nos_btn()
-                return
-            }
-            if (jogador_nao_completou_sequencia()){return}
-            fim_do_jogo()
+            resutado_da_jogada(0)
         }
         if (CLASSE_DO_BOTAO_CLICADO == 'amarelo') {
-            listaSequenciaJogador.push(1)
-            tocar_audio_correto(1)
-            if (jogada_valida()) {
-                add_random_numb_seq_maq()
-                mostrar_seq_nos_btn()
-                return
-            }
-            if (jogador_nao_completou_sequencia()){return}
-            fim_do_jogo()
+            resutado_da_jogada(1)
         }
         if (CLASSE_DO_BOTAO_CLICADO == 'vermelho') {
-            listaSequenciaJogador.push(2)
-            tocar_audio_correto(2)
-            if (jogada_valida()) {
-                add_random_numb_seq_maq()
-                mostrar_seq_nos_btn()
-                return
-            }
-            if (jogador_nao_completou_sequencia()){return}
-            fim_do_jogo()
+            resutado_da_jogada(2)
         }
         if (CLASSE_DO_BOTAO_CLICADO == 'azul') {
-            listaSequenciaJogador.push(3)
-            tocar_audio_correto(3)
-            if (jogada_valida()) {
-                add_random_numb_seq_maq()
-                mostrar_seq_nos_btn()
-                return
-            }
-            if (jogador_nao_completou_sequencia()){return}
-            fim_do_jogo()
+            resutado_da_jogada(3)
         }
     })
 }
